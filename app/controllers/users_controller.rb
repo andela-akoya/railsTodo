b3a05f05c8468cba7cc965340b2fcaed767fb4e6
Koya Gabriel <koyexes@stakeholder.local>
2017-09-12 15:01:50 +0100

    implements pagination, versioning and serialization

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   skip_before_action :authorize_request, only: :create
+  before_action :check_user_existence, only: :create
   # return authenticated token upon signup
   def create
     user = User.create!(user_params)
@@ -11,5 +12,12 @@ class UsersController < ApplicationController
   private
     def user_params
       params.permit(:name, :email, :password, :password_confirmation)
+    end
+
+    def check_user_existence
+      if User.find_by(name: user_params[:name])
+        response = { message: "User already exist"}
+        json_response(response, :bad_request)
+      end
     end
 end
